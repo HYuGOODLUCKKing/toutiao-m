034e5a277cd447cd43450fafcdcbc8de93cb295a
良.药 <10234252+good-drug@user.noreply.gitee.com>
2022-02-01 12:08:16 +0800

    项目初始化-调整初始化目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>黑马头条</h1>
+    <router-view />
   </div>
 </template>
+<script>
+export default {
+  name: 'App'
+}
+</script>
 
 <style lang="less">
 #app {
