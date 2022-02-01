9d3b94e9316c881549c675502d6da11ff18b408b
良.药 <10234252+good-drug@user.noreply.gitee.com>
2022-02-01 12:36:51 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <div><span class="iconfont icon-dianzan"></span></div>
     <router-view />
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 <script>
